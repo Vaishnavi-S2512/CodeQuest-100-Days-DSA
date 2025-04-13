ed5cc89de6bda240602aae822472eda50fea3d09
Vaishnavi-S2512 <vaishushantha@gmail.com>
2025-04-13 10:49:07 +0530

    Day2_Solution

---
+++ submissions/Vaishnavi/Day2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    char name[10];
+    printf("Hello, Cyberpunk City! ");
+    printf("enter your name");
+    scanf("%s",&name);
+    printf("Welcome to the adventure %s",name);
+    return 0;
+}
